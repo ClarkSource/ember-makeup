cdbe450da15f6e71049d1937f715338c99f4a456
Jan Buschtöns <jan.buschtoens@clark.de>
2019-05-22 14:18:58 +0200

    feat: support scss

--- ts/plugins/preprocessor-registry/pre-plugin.ts
+++ ts/plugins/preprocessor-registry/pre-plugin.ts
@@ -1,11 +1,11 @@
 import { Plugin, ToTreeOptions } from 'ember-cli-preprocessor-registry';
 import broccoliPostcss from 'broccoli-postcss';
-import cfgToVarPlugin, { Usage } from '../postcss/cfg-to-var';
 import { BroccoliNode } from 'broccoli-plugin';
 import { EmberMakeupAddon } from '../../addon';
+import { expandComponentShorthandPlugin } from '../postcss';
 
 export default class PrePlugin implements Plugin {
-  ext = ['css', 'yml'];
+  ext = ['css', 'scss'];
   name = 'ember-makeup:pre';
 
   private owner: EmberMakeupAddon;
@@ -20,20 +20,20 @@ export default class PrePlugin implements Plugin {
     _outputDirectory: string,
     _options: ToTreeOptions
   ) {
-    const cfgToVarTree = broccoliPostcss(tree, {
+    const expandComponentShorthandTree = broccoliPostcss(tree, {
       browsers: this.owner.project.targets.browsers,
       plugins: [
         {
-          module: cfgToVarPlugin,
-          options: {
-            reportUsage(usage: Usage) {
-              console.log(usage);
-            }
-          }
+          module: expandComponentShorthandPlugin
         }
       ]
     });
 
-    return this.owner.debugTree(cfgToVarTree, 'pre-plugin');
+    // Passing as part of the options object above unfortunately does not work
+    // correctly, because the `undefined` is ignored.
+    expandComponentShorthandTree.extensions = this.ext;
+    expandComponentShorthandTree.targetExtension = undefined;
+
+    return this.owner.debugTree(expandComponentShorthandTree, 'pre-plugin');
   }
 }
--- ts/types/broccoli-postcss.d.ts
+++ ts/types/broccoli-postcss.d.ts
@@ -4,7 +4,27 @@ import BroccoliPlugin, { BroccoliNode } from 'broccoli-plugin';
 export default function broccoliPostcss(
   tree: BroccoliNode,
   options?: Options
-): BroccoliPlugin;
+): BroccoliPlugin & ExtensionConfig;
+
+/**
+ * By default, if the options passed into the `Filter` constructor contain a
+ * property `extensions`, and `targetExtension` is supplied, the first matching
+ * extension in the list is replaced with the `targetExtension` option's value.
+ *
+ * @see https://github.com/stefanpenner/broccoli-persistent-filter#options
+ * @see https://github.com/jeffjewiss/broccoli-postcss/blob/f802b7ceb62d5328d4f492a622bc64f3c77e14d5/index.js#L11-L12
+ */
+interface ExtensionConfig {
+  /**
+   * An array of file extensions to process, e.g. `['css', 'scss']`
+   */
+  extensions?: string[];
+
+  /**
+   * The file extension of the corresponding output files, e.g. `'css'`.
+   */
+  targetExtension?: string;
+}
 
 interface Options {
   /**
