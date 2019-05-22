2d52464a8a1ff3d0b13bdf010d8e298a0c215333
Jan Buschtöns <jan.buschtoens@clark.de>
2019-05-22 13:49:44 +0200

    chore(deps): setup sass

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,15 +4,10 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   const app = new EmberAddon(defaults, {
-    // Add options here
+    cssModules: {
+      intermediateOutputPath: 'app/styles/css-modules.css'
+    }
   });
-
-  /*
-    This build file specifies the options for the dummy test app of this
-    addon, located in `/tests/dummy`
-    This build file does *not* influence how the addon or the app using it
-    behave. You most likely want to be modifying `./index.js` or app's build file
-  */
 
   return app.toTree();
 };
