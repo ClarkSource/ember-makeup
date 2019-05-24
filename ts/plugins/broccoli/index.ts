2a462af65f313401c81a9bca14900cfae25144b1
Jan Buschtöns <jan.buschtoens@clark.de>
2019-05-24 14:39:34 +0200

    fix(plugins/broccoli): TS6133

--- ts/plugins/broccoli/index.ts
+++ ts/plugins/broccoli/index.ts
@@ -1,7 +1,7 @@
 import BroccoliPersistentFilter from 'broccoli-persistent-filter';
 
 export default class EmberMakeupBroccoliPlugin extends BroccoliPersistentFilter {
-  processString(contents: string, relativePath: string): string | object {
+  processString(_contents: string, _relativePath: string): string | object {
     throw new Error('Method not implemented.');
   }
 }
