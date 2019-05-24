a22147c43da8a84d587ce8eb0363febe5ca61505
Jan Buschtöns <jan.buschtoens@clark.de>
2019-05-24 14:04:44 +0200

    fix: disable strict mode in `ts-node`

--- register-ts-node.js
+++ register-ts-node.js
@@ -2,7 +2,14 @@
 
 // eslint-disable-next-line node/no-deprecated-api
 if (!require.extensions['.ts']) {
-  const options = { project: `${__dirname}/ts/tsconfig.json` };
+  const options = {
+    project: `${__dirname}/ts/tsconfig.json`,
+
+    // Otherwise the `use strict` statement is inserted and toggles on strict
+    // mode, which breaks other addons, that rely on sloppy mode.
+    // In the prod build `ember-makeup` is still compiled in strict mode.
+    compilerOptions: { strict: false }
+  };
 
   // If we're operating in the context of another project, which might happen
   // if someone has installed ember-makeup from git, only perform
